9886b82445d7791a458a2d67b9e9726711cd40a3
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-02 19:25:30 +0600

    variables

--- variables.java
+++ variables.java
@@ -1,12 +1,23 @@
+import java.util.*;
+
 public class variables{
     public static void main(String[] args){
-        int a=10;
-        int b=25;
-        String name = "tony stark";
+        int a;
+        int b;
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+
+        name = sc.nextLine();
+        a = sc.nextInt();
+        b = sc.nextInt();
 
         int sum = a+b;
 
         System.out.println(name);
         System.out.println(sum);
+
+
+
     }
 }
