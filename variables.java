6d1cae05609f102b57a3490587c8c542152a9d6e
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-02 19:14:45 +0600

    variable

---
+++ variables.java
@@ -0,0 +1,12 @@
+public class variables{
+    public static void main(String[] args){
+        int a=10;
+        int b=25;
+        String name = "tony stark";
+
+        int sum = a+b;
+
+        System.out.println(name);
+        System.out.println(sum);
+    }
+}
