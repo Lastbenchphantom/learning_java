c2e5b5de0f826c0404b30f5144836df4d28be8f8
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-03 20:07:08 +0600

    take input until user wants

---
+++ NumberCounter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NumberCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+        String choice;
+
+        do {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+
+            if (number > 0) {
+                positiveCount++;
+            } else if (number < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+
+            System.out.print("Do you want to enter another number? (yes/no): ");
+            choice = scanner.next();
+        } while (choice.equalsIgnoreCase("yes"));
+
+        System.out.println("\nCount of positive numbers: " + positiveCount);
+        System.out.println("Count of negative numbers: " + negativeCount);
+        System.out.println("Count of zeros: " + zeroCount);
+
+        scanner.close();
+    }
+}
