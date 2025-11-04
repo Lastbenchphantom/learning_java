eeb81026432b2d566d6e1a95c11cad4cadae174d
Lastbenchphantom <zakariazak13913@gmail.com>
2025-11-04 16:50:46 +0600

    strings

---
+++ string1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class string1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        String array[] = new String[size];
+
+        int totallength = 0;
+
+        for(int i=0; i<size; i++){
+            array[i] = sc.next();
+            totallength += array[i].length();
+        }
+
+        System.out.println(totallength);
+    }
+}
